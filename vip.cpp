3538403d2ad438045f91c334a244009644d5ffd4
Hit <nguyenducthuank588@gmail.com>
2025-04-23 17:23:49 +0700

    add file and modified

---
+++ vip.cpp
@@ -0,0 +1,22 @@
+/*------- ndtnoob_kkk -------*/
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ldb = long double;
+using ui = unsigned;
+using ull = unsigned long long;
+
+void solve() {
+    cout << "Hello world";
+}
+
+int main() {
+    // freopen("test_input0.txt", "r", stdin);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t; cin >> t;
+    while (t--) 
+        solve();
+    return 0;
+}
